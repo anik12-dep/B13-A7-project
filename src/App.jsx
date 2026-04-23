1d5abbd83fac7f06105cb86c2f2d788b433c73c9
anik12-dep <pappudatta525@gmail.com>
2026-04-23 12:18:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
